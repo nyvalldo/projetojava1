f94be4a250284b49f29cfa22678bcdc58b9b2fdc
Nivaldo <Nivaldo@NIVALDO>
2021-12-15 22:08:03 -0300

    primeira alteração

--- primeiro-programa-java/src/cursoava/exercutavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursoava/exercutavel/PrimeiraClasseJava.java
@@ -5,7 +5,7 @@ public class PrimeiraClasseJava {
 	public static void main(String[] args) {
 		
 		int count;
-		
+		/*A primeira alteração*/
 		for(count = 0; count < 5; count++)
 			System.out.println("This is count: " + count);
 	}
