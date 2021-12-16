8d5a034bbb926dbbc41ad224fae8e3268517b5a6
Nivaldo <Nivaldo@NIVALDO>
2021-12-15 21:44:39 -0300

    Criação do projeto

---
+++ primeiro-programa-java/src/cursoava/exercutavel/PrimeiraClasseJava.java
@@ -0,0 +1,12 @@
+package cursoava.exercutavel;
+
+public class PrimeiraClasseJava {
+	
+	public static void main(String[] args) {
+		
+		int count;
+		
+		for(count = 0; count < 5; count++)
+			System.out.println("This is count: " + count);
+	}
+}
