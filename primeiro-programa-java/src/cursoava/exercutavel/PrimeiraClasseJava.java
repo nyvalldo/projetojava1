a95a842cc09ac4f386ad1636d1ca8b989c2ed256
Nivaldo <Nivaldo@NIVALDO>
2021-12-22 21:02:49 -0300

    Terceira alteração

--- primeiro-programa-java/src/cursoava/exercutavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursoava/exercutavel/PrimeiraClasseJava.java
@@ -5,6 +5,7 @@ public class PrimeiraClasseJava {
 	public static void main(String[] args) {
 		
 		int count;
+		int soma = 0;
 		/*A primeira alteração*/
 		for(count = 0; count < 5; count++)
 			System.out.println("This is count: " + count);
